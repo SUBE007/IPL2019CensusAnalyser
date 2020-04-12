a14380aa6c5311fbf9c2cc8c1bbbcbdca5de8529
SUBE007 <chaurasiyasube007@gmail.com>
2020-04-12 18:05:09 +0530

    added Test Case for the validation of wrong csv filepath

--- src/test/java/IPLAnalyserTest.java
+++ src/test/java/IPLAnalyserTest.java
@@ -5,11 +5,12 @@ import ipl2019census.MostRunCSV;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class IPLAnalyserTest {
     public static String IPL_CENSUS_CSV_MOSTRUNS_FILEPATH="E:\\BridgrLabz\\IPL2019Census\\src\\test\\resources\\IPL2019FactsheetMostRuns.csv";
     public static String IPL_CENSUS_CSV_MOSTWICKETS_CSV_FILEPATH="E:\\BridgrLabz\\IPL2019Census\\src\\test\\resources\\IPL2019FactsheetMostWkts.csv";
-
+    public static String  WRONG_CSV_FILE_PATH="E:\\BridgrLabz\\IPL2019Census\\src\\IPL2019FactsheetMostRuns.csv";
     IPLCensusAnalyser censusAnalyser;
 
     @Before
@@ -29,12 +30,25 @@ public class IPLAnalyserTest {
 
     @Test
     public void givenIPLCensusMostRunsData_ShouldReturnTopBattingAverages_WhenSortedOnIt() {
-        try{ censusAnalyser.loadIPLMostRunsData(IPL_CENSUS_CSV_MOSTRUNS_FILEPATH);
+        try{
+            censusAnalyser.loadIPLMostRunsData(IPL_CENSUS_CSV_MOSTRUNS_FILEPATH);
             String playersData =  censusAnalyser.getAverageSortedRunOfIpl();
             MostRunCSV[] AverageRuns = new Gson().fromJson(playersData, MostRunCSV[].class);
             Assert.assertEquals("MS Dhoni", AverageRuns[AverageRuns.length-1].player);
         }catch (IPLCSVException e){
             e.printStackTrace();
        }
+    }
+
+    @Test
+    public void givenIplCensusMostRunsData_WithWrongFile_ShouldThrowException() {
+        try {
+
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(IPLCSVException.class);
+            censusAnalyser.loadIPLMostRunsData(WRONG_CSV_FILE_PATH);
+        } catch (IPLCSVException e) {
+            Assert.assertEquals(IPLCSVException.ExceptionType.NO_SUCH_FILE,e.type);
+        }
     }
 }
