5ccafaf65bac1d10901f938866bb5f58346d1a94
SUBE007 <chaurasiyasube007@gmail.com>
2020-04-12 20:50:00 +0530

    added functionality to find BatsMan Who has Great Avg run rate with Maximum Run in IPL-2019

--- src/main/java/ipl2019census/SortedField.java
+++ src/main/java/ipl2019census/SortedField.java
@@ -6,8 +6,8 @@ import java.util.Map;
 
 public class SortedField {
     public enum Field {
-         AVERAGE,STRIKE_RATE,SIXES_AND_FOURS,CENTUARY, FOURS, HALFCENTUARY, HIGHSCORE, SIX, RUN,
-        SIX_AND_FOUR_WITH_STRIKERATE,GREATAVG_WITH_STRIKERATE;
+        AVERAGE,STRIKE_RATE,SIXES_AND_FOURS,CENTUARY, FOURS, HALFCENTUARY, HIGHSCORE, SIX, RUN,
+        SIX_AND_FOUR_WITH_STRIKERATE,GREATAVG_WITH_STRIKERATE,MAXRUN_WITH_GREATAVG;
     }
     static Map<Field, Comparator> sortFieldComparator = new HashMap<>();
 
@@ -17,10 +17,11 @@ public class SortedField {
     {
         Comparator<MostRunCSV> averageComparator = Comparator.comparing(census -> census.avg);
         Comparator<MostRunCSV> strikeRateComparator = Comparator.comparing(census -> census.strikeRate);
+        Comparator<MostRunCSV> maxrunComparator = Comparator.comparing(census -> census.run);
         sortFieldComparator.put(Field.AVERAGE, averageComparator);
         sortFieldComparator.put(Field.STRIKE_RATE, strikeRateComparator);
         sortFieldComparator.put(Field.SIXES_AND_FOURS, new FieldComparator());
-        sortFieldComparator.put(Field.GREATAVG_WITH_STRIKERATE, averageComparator.thenComparing(strikeRateComparator));
+        sortFieldComparator.put(Field.MAXRUN_WITH_GREATAVG, maxrunComparator.thenComparing(averageComparator));
         Comparator<MostRunCSV> csvComparator = sortFieldComparator.get(field);
         return csvComparator;
     }
