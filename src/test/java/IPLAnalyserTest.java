aefa0c2e5b39a7c7761b78c2ef66da96bec3040d
SUBE007 <chaurasiyasube007@gmail.com>
2020-04-12 18:20:05 +0530

    added Test Case for the validation of wrong Delimetre csv file

--- src/test/java/IPLAnalyserTest.java
+++ src/test/java/IPLAnalyserTest.java
@@ -8,9 +8,10 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 public class IPLAnalyserTest {
-    public static String IPL_CENSUS_CSV_MOSTRUNS_FILEPATH="E:\\BridgrLabz\\IPL2019Census\\src\\test\\resources\\IPL2019FactsheetMostRuns.csv";
-    public static String IPL_CENSUS_CSV_MOSTWICKETS_CSV_FILEPATH="E:\\BridgrLabz\\IPL2019Census\\src\\test\\resources\\IPL2019FactsheetMostWkts.csv";
+    public static String IPL_CENSUS_CSV_MOSTRUNS_FILEPATH="resources/IPL2019FactsheetMostRuns.csv";
+    public static String IPL_CENSUS_CSV_MOSTWICKETS_CSV_FILEPATH="resources/IPL2019FactsheetMostWkts.csv";
     public static String  WRONG_CSV_FILE_PATH="E:\\BridgrLabz\\IPL2019Census\\src\\IPL2019FactsheetMostRuns.csv";
+    public static String IPL_CENSUS_CSV_MOSTRUNS_FILEPATH_WRONGDELIMETER ="resources/IPL2019FactsheetMostRunsWithWrongDelimiter.csv";
     IPLCensusAnalyser censusAnalyser;
 
     @Before
@@ -49,6 +50,17 @@ public class IPLAnalyserTest {
             censusAnalyser.loadIPLMostRunsData(WRONG_CSV_FILE_PATH);
         } catch (IPLCSVException e) {
             Assert.assertEquals(IPLCSVException.ExceptionType.NO_SUCH_FILE,e.type);
+        }
+    }
+
+    @Test
+    public void givenIplCensusMostRunsData_WithWrongDelimeter_ShouldThrowException() {
+        try {
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(IPLCSVException.class);
+            censusAnalyser.loadIPLMostRunsData(IPL_CENSUS_CSV_MOSTRUNS_FILEPATH_WRONGDELIMETER);
+        } catch (IPLCSVException e) {
+             Assert.assertEquals(IPLCSVException.ExceptionType.ISSUE_IN_FILE,e.type);
         }
     }
 }
